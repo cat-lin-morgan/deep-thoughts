b5ff8dd282c14cae992a827de906d78e4e1e98c5
cat - lin <plummorgan@gmail.com>
2021-01-15 00:49:47 -0800

    query single thought

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,13 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
@@ -11,15 +18,26 @@ const client = new ApolloClient({
 });
 
 function App() {
+ 
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
-        </div>
-        <Footer />
-      </div>
+      <Router>
+        <Switch>
+          <div className='flex-column justify-flex-start min-100-vh'>
+            <Header />
+            <div className='container'>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={Signup} />
+              <Route exact path='/profile/:username' component={Profile} />
+              <Route exact path='/thought/:id' component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </div>
+            <Footer />
+          </div>
+        </Switch>
+      </Router>
     </ApolloProvider>
   );
 }
