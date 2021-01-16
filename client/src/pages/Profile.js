5b29b27c3e1aeb4b0f8554cbd8f60c31216a044d
cat - lin <plummorgan@gmail.com>
2021-01-16 10:05:44 -0800

    added thoughttext component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,26 +1,26 @@
 import React from 'react';
 
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 import { Redirect, useParams } from 'react-router-dom';
 
 import Auth from '../utils/auth';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Profile = () => {
   const { username: userParam } = useParams();
 
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
-  console.log('hio', data);
-  console.log(userParam);
 
   const user = data?.me || data?.user || {};
-  console.log('eeee', Auth.loggedIn(), Auth.getProfile().data.username, userParam);
   if (Auth.loggedIn() && '' === userParam) {
     const profile = Auth.getProfile().data.username;
     return <Redirect to={`/profile/${profile}`} />
@@ -38,12 +38,27 @@ const Profile = () => {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -52,6 +67,8 @@ const Profile = () => {
         </div>
       </div>
 
+        <div className="mb-3">{!userParam && <ThoughtForm />}</div>
+
       <div className="col-12 col-lg-3 mb-3">
         <FriendList
           username={user.username}
@@ -59,6 +76,7 @@ const Profile = () => {
           friends={user.friends}
         />
       </div>
+
     </div>
   );
 };
